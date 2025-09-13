6399a74de45ab4eeabedf3125dbaf0f4d5c1831b
fredy estiven diaz pinilla <fredyestivendiazpinilla3@gmail.com>
2025-09-12 20:57:58 -0500

    creacion de vista modelo controlado

---
+++ config/database.php
@@ -0,0 +1,13 @@
+<?php
+
+
+class database{
+    public static function conectar  (){
+        $conexion = new mysqli("localhost","root","", "tienda2");
+        $conexion->query("SET NAMES 'utf8'");
+        return $conexion;
+     
+
+    }
+
+}
